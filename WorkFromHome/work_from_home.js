3969742302c930222161c070ec11ae3b222dac49
Chandan Mourya <chandanmourya4786@gmail.com>
2022-04-02 17:31:47 +0530

    Work from page completed in this folder

---
+++ WorkFromHome/work_from_home.js
@@ -0,0 +1,302 @@
+
+var JobData = [
+    {
+        "Role": "Brand Development Associate, Launchpad",
+        "CompanyName": "Amazon",
+        "Location": "Bengaluru / Bangalore , India",
+        "Experience": " 1-4 years",
+        "Salary": "Not Specified",
+        "Description": "Emerging Brands Program team's mission is to build the most effective platform for motivated entrepreneurs to establish & scale consumer direc...."
+    },
+    {
+        "Role": "Manager, Support Engineering , Partner Trust",
+        "CompanyName": "Amazon",
+        "Location": "Bengaluru / Bangalore , India",
+        "Experience": " 3-6 years",
+        "Salary": "Not Specified",
+        "Description": "Amazon is investing heavily in building a world class advertising business and we are responsible for defining and delivering a collection of self-service performance advertising products that drive discovery and sales...."
+    },
+    {
+        "Role": "Software Development Engineer II",
+        "CompanyName": "Amazon",
+        "Location": "Bengaluru / Bangalore , India",
+        "Experience": " 2-5 years",
+        "Salary": "Not Specified",
+        "Description": "Amazon Payment Experience Platform team at Amazon India Development Center, Bangalore is looking for a Developer to design and build the next generation of Payments platform and product from the ground up..."
+    },
+    {
+        "Role": "Business Intelligence Engineer, IN Advertising",
+        "CompanyName": "Amazon",
+        "Location": "Bengaluru / Bangalore",
+        "Experience": " 4-7 years",
+        "Salary": " Not Specified",
+        "Description": "Amazon Advertising exists at the intersection of marketing and e-commerce and offers advertisers a rich array of innovative advertising solutions across Amazon owned properties as well as third party properties Amazon operates....."
+    },
+    {
+        "Role": "Risk Control Analyst",
+        "CompanyName": "Amazon",
+        "Location": "Bengaluru / Bangalore , India",
+        "Experience": " 1-4 years",
+        "Salary": "Not Specified",
+        "Description": "Suspicious Activity Monitoring (SAM) enables Amazon businesses to mitigate the regulatory and legal risk of money laundering and terrorist financing...."
+    },
+    {
+        "Role": "Software Development Engineer, Alexa Smart Properties",
+        "CompanyName": "Amazon",
+        "Location": "Bengaluru / Bangalore , India",
+        "Experience": "Not Specified",
+        "Salary": " Not Specified",
+        "Description": "Are you looking for an opportunity to build a highly available, large scale, enterprise-wide software solution Does it excite you to find patterns and build generic, composable....."
+    },
+    {
+        "Role": "Network Specialist",
+        "CompanyName": "Standard Chartered",
+        "Location": " Bengaluru / Bangalore ,India",
+        "Experience": "10-15 Years",
+        "Salary": " Not Specified",
+        "Description": "We are a leading international bank focused on helping people and companies prosper across Asia, Africa and the Middle East......"
+    },
+    {
+        "Role": "Relationship Manager,Priority Banking",
+        "CompanyName": "Standard Chartered",
+        "Location": " Bengaluru / Bangalore ,India",
+        "Experience": "Not Specified",
+        "Salary": " Not Specified",
+        "Description": "We're an international bank, nimble enough to act, big enough for impact. For more than 160 years, we've worked to make a positive difference for our clients, communities, and each other...."
+    },
+    {
+        "Role": "Vice President, Client Coverage Performance Reporting",
+        "CompanyName": "Standard Chartered",
+        "Location": "Bengaluru / Bangalore ,India",
+        "Experience": "Not Specified",
+        "Salary": "Not Specified",
+        "Description": "We're an international bank, nimble enough to act, big enough for impact. For more than 160 years, we've worked to make a positive difference for our clients, communities,......"
+    },
+    {
+        "Role": "Design Engineer- Creo , Plastics",
+        "CompanyName": "Harita Techserv Private Limited",
+        "Location": " Bengaluru / Bangalore",
+        "Experience": "3-8 Years",
+        "Salary": "Not Specified",
+        "Description": "ands on experience in Creo CAD Software with experience in various modules Top down Design Approach/ Part / Assembly/ Drafting/AdvanceSurfacing/ Assembly work instructions...."
+    },
+    {
+        "Role": "Design Engineer - Solidworks - Bangalore",
+        "CompanyName": "Harita Techserv Private Limited",
+        "Location": " Bengaluru / Bangalore",
+        "Experience": "4-9 Years",
+        "Salary": "Not Specified",
+        "Description": "Knowledge of GD&T and drafting standards. Knowledge of sheet metals, castings, forgings....."
+    },
+    {
+        "Role": "Cnc Maintenance Engineer - Bangalore",
+        "CompanyName": "Harita Techserv Private Limited",
+        "Location": " Bengaluru / Bangalore",
+        "Experience": "2-6 Years",
+        "Salary": " Not Specified",
+        "Description": "Maintenance of CNC & Grinding machines Adherence to schedule maintenance........"
+    },
+    {
+        "Role": "Brand Development Associate, Launchpad",
+        "CompanyName": "Amazon",
+        "Location": "Bengaluru / Bangalore , India",
+        "Experience": " 1-4 years",
+        "Salary": "Not Specified",
+        "Description": "Emerging Brands Program team's mission is to build the most effective platform for motivated entrepreneurs to establish & scale consumer direc...."
+    },
+    {
+        "Role": "Manager, Support Engineering , Partner Trust",
+        "CompanyName": "Amazon",
+        "Location": "Bengaluru / Bangalore , India",
+        "Experience": " 3-6 years",
+        "Salary": "Not Specified",
+        "Description": "Amazon is investing heavily in building a world class advertising business and we are responsible for defining and delivering a collection of self-service performance advertising products that drive discovery and sales...."
+    },
+    {
+        "Role": "Software Development Engineer II",
+        "CompanyName": "Amazon",
+        "Location": "Bengaluru / Bangalore , India",
+        "Experience": " 2-5 years",
+        "Salary": "Not Specified",
+        "Description": "Amazon Payment Experience Platform team at Amazon India Development Center, Bangalore is looking for a Developer to design and build the next generation of Payments platform and product from the ground up..."
+    },
+    {
+        "Role": "Business Intelligence Engineer, IN Advertising",
+        "CompanyName": "Amazon",
+        "Location": "Bengaluru / Bangalore",
+        "Experience": " 4-7 years",
+        "Salary": " Not Specified",
+        "Description": "Amazon Advertising exists at the intersection of marketing and e-commerce and offers advertisers a rich array of innovative advertising solutions across Amazon owned properties as well as third party properties Amazon operates....."
+    },
+    {
+        "Role": "Risk Control Analyst",
+        "CompanyName": "Amazon",
+        "Location": "Bengaluru / Bangalore , India",
+        "Experience": " 1-4 years",
+        "Salary": "Not Specified",
+        "Description": "Suspicious Activity Monitoring (SAM) enables Amazon businesses to mitigate the regulatory and legal risk of money laundering and terrorist financing...."
+    },
+    {
+        "Role": "Software Development Engineer, Alexa Smart Properties",
+        "CompanyName": "Amazon",
+        "Location": "Bengaluru / Bangalore , India",
+        "Experience": "Not Specified",
+        "Salary": " Not Specified",
+        "Description": "Are you looking for an opportunity to build a highly available, large scale, enterprise-wide software solution Does it excite you to find patterns and build generic, composable....."
+    },
+    {
+        "Role": "Network Specialist",
+        "CompanyName": "Standard Chartered",
+        "Location": " Bengaluru / Bangalore ,India",
+        "Experience": "10-15 Years",
+        "Salary": " Not Specified",
+        "Description": "We are a leading international bank focused on helping people and companies prosper across Asia, Africa and the Middle East......"
+    },
+    {
+        "Role": "Relationship Manager,Priority Banking",
+        "CompanyName": "Standard Chartered",
+        "Location": " Bengaluru / Bangalore ,India",
+        "Experience": "Not Specified",
+        "Salary": " Not Specified",
+        "Description": "We're an international bank, nimble enough to act, big enough for impact. For more than 160 years, we've worked to make a positive difference for our clients, communities, and each other...."
+    },
+    {
+        "Role": "Vice President, Client Coverage Performance Reporting",
+        "CompanyName": "Standard Chartered",
+        "Location": "Bengaluru / Bangalore ,India",
+        "Experience": "Not Specified",
+        "Salary": "Not Specified",
+        "Description": "We're an international bank, nimble enough to act, big enough for impact. For more than 160 years, we've worked to make a positive difference for our clients, communities,......"
+    },
+    {
+        "Role": "Design Engineer- Creo , Plastics",
+        "CompanyName": "Harita Techserv Private Limited",
+        "Location": " Bengaluru / Bangalore",
+        "Experience": "3-8 Years",
+        "Salary": "Not Specified",
+        "Description": "ands on experience in Creo CAD Software with experience in various modules Top down Design Approach/ Part / Assembly/ Drafting/AdvanceSurfacing/ Assembly work instructions...."
+    },
+    {
+        "Role": "Design Engineer - Solidworks - Bangalore",
+        "CompanyName": "Harita Techserv Private Limited",
+        "Location": " Bengaluru / Bangalore",
+        "Experience": "4-9 Years",
+        "Salary": "Not Specified",
+        "Description": "Knowledge of GD&T and drafting standards. Knowledge of sheet metals, castings, forgings....."
+    },
+    {
+        "Role": "Cnc Maintenance Engineer - Bangalore",
+        "CompanyName": "Harita Techserv Private Limited",
+        "Location": " Bengaluru / Bangalore",
+        "Experience": "2-6 Years",
+        "Salary": " Not Specified",
+        "Description": "Maintenance of CNC & Grinding machines Adherence to schedule maintenance........"
+    },
+    {
+        "Role": "Software Development Engineer II",
+        "CompanyName": "Amazon",
+        "Location": "Bengaluru / Bangalore , India",
+        "Experience": " 2-5 years",
+        "Salary": "Not Specified",
+        "Description": "Amazon Payment Experience Platform team at Amazon India Development Center, Bangalore is looking for a Developer to design and build the next generation of Payments platform and product from the ground up..."
+    }
+]
+
+// var Data = JSON.parse(JobData)
+
+JobData.map(function(elem,index){
+
+    var mainbox = document.createElement("div");
+    mainbox.setAttribute("id","mainbox")
+
+    var div = document.createElement("div");
+    div.setAttribute("id","upper-div")
+
+    var div1 = document.createElement("div");
+    var h3 = document.createElement("h3");
+    h3.setAttribute("class","black role")
+    h3.innerText = elem.Role
+    div1.append(h3)
+
+    var div2 = document.createElement("div");
+    var p = document.createElement("p");
+    p.setAttribute("class","blue")
+    p.innerText = elem.CompanyName
+    div2.append(p)
+
+    var div3 = document.createElement("div");
+    div3.setAttribute("class","box")
+
+    var div3_1 = document.createElement("div");
+    div3_1.innerHTML = '<i class="fa-solid fa-location-dot"></i>';
+    var span1 = document.createElement("span");
+    span1.setAttribute("class","icon black")
+    span1.innerText = elem.Location
+    div3_1.append(span1)
+    div3_1.setAttribute("class","loc")
+
+    var div3_2 = document.createElement("div");
+    div3_2.innerHTML = '<i class="fa-solid fa-briefcase"></i>';
+    var span2 = document.createElement("span");
+    span2.setAttribute("class","icon black")
+    span2.innerText = elem.Experience
+    div3_2.append(span2)
+    div3_2.setAttribute("class","experience")
+
+    var div3_3 = document.createElement("div");
+    div3_3.innerHTML = '<i class="fa-solid fa-coins"></i>'
+    var span3 = document.createElement("span");
+    span3.setAttribute("class","icon black")
+    span3.innerText = elem.Salary
+    div3_3.append(span3)
+    div3_3.setAttribute("class","sal")
+
+    div3.append(div3_1,div3_2,div3_3)
+
+    var div4 = document.createElement("div");
+    var p1 = document.createElement("p");
+    p1.setAttribute("id","desc")
+    p1.innerText = elem.Description
+    div4.append(p1)
+
+    var div5 = document.createElement("div");
+    div5.setAttribute("id","lower-div")
+
+    var div5_1 = document.createElement("div");
+    var p2 = document.createElement("p");
+    p2.innerText = "Posted: 15 minutes ago |"
+    p2.style.color = "darkslategray"
+    var span7 = document.createElement("span");
+    span7.innerText = "Work From Home | Jobs For COVID-19 Layoffs"
+    span7.style.marginLeft = "10px"
+    span7.style.color = "rgb(112, 94, 230)"
+    p2.append(span7)
+    div5_1.append(p2)
+
+    var div5_2 = document.createElement("div");
+    var span4 = document.createElement("span");
+    span4.innerHTML = '<i class="fa-regular fa-star"></i>'    
+    
+    var span5 = document.createElement("span");
+    span5.innerHTML = '<i class="fa-solid fa-share-nodes"></i>'
+    span5.setAttribute("class","icon")
+
+    var span6 = document.createElement("span");
+    var btn = document.createElement("button");
+    btn.setAttribute("id","btn")
+    btn.innerText = "APPLY"
+    span6.setAttribute("class","icon")
+    span6.append(btn)
+
+    div5_2.append(span4,span5,span6)
+    div5.append(div5_1,div5_2)
+
+    div.append(div1,div2,div3,div4)
+
+    mainbox.append(div,div5)
+
+    document.querySelector("#job-box").append(mainbox)
+
+    document.querySelector("#length").innerText = `${JobData.length} Work From Home Jobs in India`
+})
+
